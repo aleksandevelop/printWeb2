fb4aced7cc44fc3b5763b3392ec345296decc9bb
Aleksander Belov <bsaska86@gmail.com>
2026-01-06 23:33:02 +0300

    add normal burger button and edit contact form

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,8 +3,10 @@ import { initCatalogFilter } from './catalog-filter.js';
 import { initSeeAllButton } from './see-all-button.js';
 import { initInputMask } from './inputMask.js';
 import { initBurgerMenu } from './burger-menu.js';
+import Header from './header.js'
 
 document.addEventListener("DOMContentLoaded", function() {
+  new Header()
   initCatalogFilter();
   initSeeAllButton();
   initInputMask();
